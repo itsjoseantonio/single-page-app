85de8629dc343c30b1394e616643e9fcaf1e6a42
itsjoseantonio <jos.valga@gmail.com>
2021-11-01 16:32:24 -0500

    pwa

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/global/index.scss';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
     <>
@@ -9,3 +10,5 @@ ReactDOM.render(
     </>,
     document.getElementById('root')
 );
+
+serviceWorkerRegistration.register();
